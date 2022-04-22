e8b7231f2d2a353e1b41bb54432c1a9d212474d5
lauren-briggs <laurenbriggs41@gmail.com>
2022-04-22 12:52:02 +1000

    added imgs to branding and branding js file

---
+++ branding.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+
+
+    // editorial menu function - change img on hover
+    const projectCont = $('.branding-menu-item');
+    // var edImgCont = $('#editorial-img-cont');
+    var imgChange
+    // var change = $('#change');
+    // var pauseIcon = $('#pause')
+    // var aboutMove = $('#about-move');
+    // var logo = $('#logo');
+
+    projectCont.mouseover(function () {
+        if (this.id == 'branding-menu-wilson') {
+            imgChange = $("#branding-menu-wilson");
+            imgChange.addClass('showme');
+        } else if (this.id == 'branding-menu-tdr') {
+            imgChange = $("#branding-menu-tdr");
+            imgChange.addClass('showme');
+        } else if (this.id == 'branding-menu-am') {
+            imgChange = $("#branding-menu-am");
+            imgChange.addClass('showme');
+        } else if (this.id == 'branding-menu-lampedusa') {
+            imgChange = $("#branding-menu-lampedusa");
+            imgChange.addClass('showme');
+        } else if (this.id == 'branding-menu-boozery') {
+            imgChange = $("#branding-menu-boozery");
+            imgChange.addClass('showme');
+        } else if (this.id == 'branding-menu-frosty') {
+            imgChange = $("#branding-menu-frosty");
+            imgChange.addClass('showme');
+        } else {
+            this.removeClass('showme');
+        }
+    });
+    projectCont.mouseleave(function () {
+        if (this.id == 'branding-menu-wilson') {
+            imgChange = $("#branding-menu-wilson");
+            imgChange.removeClass('showme');
+        } else if (this.id == 'branding-menu-tdr') {
+            imgChange = $("#branding-menu-tdr");
+            imgChange.removeClass('showme');
+        } else if (this.id == 'branding-menu-am') {
+            imgChange = $("#branding-menu-am");
+            imgChange.removeClass('showme');
+        } else if (this.id == 'branding-menu-lampedusa') {
+            imgChange = $("#branding-menu-lampedusa");
+            imgChange.removeClass('showme');
+        } else if (this.id == 'branding-menu-boozery') {
+            imgChange = $("#branding-menu-boozery");
+            imgChange.removeClass('showme');
+        } else if (this.id == 'branding-menu-frosty') {
+            imgChange = $("#branding-menu-frosty");
+            imgChange.removeClass('showme');
+        } else {
+            this.removeClass('showme');
+        }
+    });
+
+    // pause on hover
+
+
+});
